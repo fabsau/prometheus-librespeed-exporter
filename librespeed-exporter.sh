d3321585b00a8ea2a1e061d2344a4036fe2ac492
fabio <github@sauna.re>
2023-12-23 03:28:00 +0100

    Redo of main script: - Add description for each metric - Add more metrics (IP, Hostname, Location, ISP Info - Improved customaziblity by allowing CUSTOM_ARGS - Renamed variables for better clarity - Allowed testing both public server and custom ones - Add checks and errors for faulty configuration

--- librespeed-exporter.sh
+++ librespeed-exporter.sh
@@ -1,111 +1,118 @@
 #!/bin/bash
 
-BACKENDS_JSON_FILE=/librespeed-backends.json
+# Function to get metrics from the JSON
+get_metric_values() {
+    local JSON=$1
 
-if [ "$PERFORM_DOWNLOAD" == "FALSE" ]
-then
-	DOWNLOAD_SWITCH=--no-download
-fi
+    local entries=$(echo "$JSON" | jq -r '. | length') # Get the count of entries in JSON
 
-if [ "$PERFORM_UPLOAD" == "FALSE" ]
-then
-	UPLOAD_SWITCH=--no-upload
-fi
+	# For every entry in JSON, extract required values and print them in Prometheus Exposition format
+    for ((i=0;i<$entries;i++)); do
+        local SERVER=$(jq -r ".[$i].server.name" <<< "$JSON") # Server name
 
-echo $DOWNLOAD_SWITCH
-echo $UPLOAD_SWITCH
+		# Extract various server metric values
+        local ID=$(jq -r ".[$i].server.id" <<< "$JSON")
+        local URL=$(jq -r ".[$i].server.url" <<< "$JSON")
+        local DOWNLOAD=$(jq -r ".[$i].download" <<< "$JSON")
+        local UPLOAD=$(jq -r ".[$i].upload" <<< "$JSON")
+        local PING=$(jq -r ".[$i].ping" <<< "$JSON")
+        local JITTER=$(jq -r ".[$i].jitter" <<< "$JSON")
+        local BYTES_SENT=$(jq -r ".[$i].bytes_sent" <<< "$JSON")
+        local BYTES_RECEIVED=$(jq -r ".[$i].bytes_received" <<< "$JSON")
 
-if [ -z "$SERVER_IDS" ]
-then
-	echo "# No server's specified"
+		# Extract client metric values
+        local IP=$(jq -r ".[$i].client.ip" <<< "$JSON")
+        local HOSTNAME=$(jq -r ".[$i].client.hostname" <<< "$JSON")
+        local CITY=$(jq -r ".[$i].client.city" <<< "$JSON")
+        local POSTAL=$(jq -r ".[$i].client.postal" <<< "$JSON")
+        local REGION=$(jq -r ".[$i].client.region" <<< "$JSON")
+        local COUNTRY=$(jq -r ".[$i].client.country" <<< "$JSON")
+        local LOC=$(jq -r ".[$i].client.loc" <<< "$JSON")
+        local ORG=$(jq -r ".[$i].client.org" <<< "$JSON")
+        local TIMEZONE=$(jq -r ".[$i].client.timezone" <<< "$JSON")
 
-	# Get the result
-	if [ -f "$BACKENDS_JSON_FILE" ]
-	then
-		echo "# Using custom backends file"
-		JSON=$(librespeed-cli $DOWNLOAD_SWITCH $UPLOAD_SWITCH --local-json $BACKENDS_JSON_FILE --json)
-	else
-		echo "# Not using custom backends file"
-		JSON=$(librespeed-cli $DOWNLOAD_SWITCH $UPLOAD_SWITCH --json)
-	fi
+		# Print metrics in Prometheus format
+		echo "# HELP librespeed_server_info Information about the Librespeed server."
+		echo "# TYPE librespeed_server_info gauge"
+		echo "librespeed_server_info{server=\"$SERVER\", url=\"$URL\"} 1"
 
-	echo "# JSON: $JSON"
+		echo "# HELP librespeed_download Download speed in Mbps."
+		echo "# TYPE librespeed_download gauge"
+		echo "librespeed_download{server=\"$SERVER\"} $DOWNLOAD"
 
-	# Parse out the values
-	SERVER=$(echo $JSON | jq ".server.name")
-	BYTES_SENT=$(echo $JSON | jq ".bytes_sent")
-	BYTES_RECEIVED=$(echo $JSON | jq ".bytes_received")
-	PING=$(echo $JSON | jq ".ping")
-	JITTER=$(echo $JSON | jq ".jitter")
-	UPLOAD=$(echo $JSON | jq ".upload")
-	DOWNLOAD=$(echo $JSON | jq ".download")
+		echo "# HELP librespeed_upload Upload speed in Mbps."
+		echo "# TYPE librespeed_upload gauge"
+		echo "librespeed_upload{server=\"$SERVER\"} $UPLOAD"
 
-        echo "# SERVER $SERVER"
+		echo "# HELP librespeed_ping Ping in ms."
+		echo "# TYPE librespeed_ping gauge"
+		echo "librespeed_ping{server=\"$SERVER\"} $PING"
 
-	# Produce the export line
-	echo librespeed_bytes_sent{server=$SERVER} $BYTES_SENT
-	echo librespeed_bytes_received{server=$SERVER} $BYTES_RECEIVED
-	echo librespeed_ping{server=$SERVER} $PING
-	echo librespeed_jitter{server=$SERVER} $JITTER
+		echo "# HELP librespeed_jitter Jitter in ms."
+		echo "# TYPE librespeed_jitter gauge"
+		echo "librespeed_jitter{server=\"$SERVER\"} $JITTER"
 
-	if [ "$PERFORM_DOWNLOAD" != "FALSE" ]
-	then
-		echo "# Emitting download metric"
-		echo librespeed_download{server=$SERVER} $DOWNLOAD
-	fi
+		echo "# HELP librespeed_bytes_received Bytes received during the test."
+		echo "# TYPE librespeed_bytes_received gauge"
+		echo "librespeed_bytes_received{server=\"$SERVER\"} $BYTES_RECEIVED"
 
-	if [ "$PERFORM_UPLOAD" != "FALSE" ]
-	then
-		echo "# Emitting upload metric"
-		echo librespeed_upload{server=$SERVER} $UPLOAD
-	fi
-else
-	echo "# Server's specified: $SERVER_IDS"
+		echo "# HELP librespeed_bytes_sent Bytes sent during the test."
+		echo "# TYPE librespeed_bytes_sent gauge"
+		echo "librespeed_bytes_sent{server=\"$SERVER\"} $BYTES_SENT"
 
-	IFS='|' read -ra SERVER_ID_ARRAY <<< "$SERVER_IDS"
+		echo "# HELP librespeed_client_info Information about the Librespeed client."
+		echo "# TYPE librespeed_client_info gauge"
+		echo "librespeed_client_info{server=\"$SERVER\", ip=\"$IP\", hostname=\"$HOSTNAME\", org=\"$ORG\"} 1"
 
-	for SERVER_ID in "${SERVER_ID_ARRAY[@]}"
-	do
-	        echo "# SERVER_ID $SERVER_ID"
-
-		# Get the result
-		if [ -f "$BACKENDS_JSON_FILE" ]
-		then
-			echo "# Using custom backends file"
-			JSON=$(librespeed-cli $DOWNLOAD_SWITCH $UPLOAD_SWITCH --local-json $BACKENDS_JSON_FILE --server $SERVER_ID --json)
-		else
-			echo "# Not using custom backends file"
-			JSON=$(librespeed-cli $DOWNLOAD_SWITCH $UPLOAD_SWITCH --json)
-		fi
-
-		echo "# JSON: $JSON"
-
-		# Parse out the values
-		SERVER=$(echo $JSON | jq ".server.name")
-		BYTES_SENT=$(echo $JSON | jq ".bytes_sent")
-		BYTES_RECEIVED=$(echo $JSON | jq ".bytes_received")
-		PING=$(echo $JSON | jq ".ping")
-		JITTER=$(echo $JSON | jq ".jitter")
-		DOWNLOAD=$(echo $JSON | jq ".download")
-		UPLOAD=$(echo $JSON | jq ".upload")
-
-	        echo "# SERVER $SERVER"
-
-		# Produce the export line
-		echo librespeed_bytes_sent{server=$SERVER} $BYTES_SENT
-		echo librespeed_bytes_received{server=$SERVER} $BYTES_RECEIVED
-		echo librespeed_ping{server=$SERVER} $PING
-		echo librespeed_jitter{server=$SERVER} $JITTER
-
-		if [ "$PERFORM_DOWNLOAD" != "FALSE" ]
-		then
-			echo librespeed_download{server=$SERVER} $DOWNLOAD
-		fi
-
-		if [ "$PERFORM_UPLOAD" != "FALSE" ]
-		then
-			echo librespeed_upload{server=$SERVER} $UPLOAD
-		fi
+		echo "# HELP librespeed_client_location_info Location information about the Librespeed client."
+		echo "# TYPE librespeed_client_location_info gauge"
+		echo "librespeed_client_location_info{server=\"$SERVER\", city=\"$CITY\", postal=\"$POSTAL\", region=\"$REGION\", country=\"$COUNTRY\", loc=\"$LOC\", timezone=\"$TIMEZONE\"} 1"
 	done
+}
+
+# This function runs the librespeed-cli test and then calls get_metric_values with the result.
+run_tests() {
+    local TEST_RESULT=$(librespeed-cli --json $1)
+    get_metric_values "$TEST_RESULT"
+}
+
+# Fetch ids from CUSTOM_SERVER_FILE if file exists and has content
+declare -A VALID_IDS=()
+all_ids=""
+if [[ -f "$CUSTOM_SERVER_FILE" ]] && [[ -s "$CUSTOM_SERVER_FILE" ]]; then
+	# Read each id from the file and store it in a map and a comma-separated string
+  while read -r id; do
+    VALID_IDS[$id]=1
+    all_ids+="${id},"
+  done < <(jq -r '.[].id' "$CUSTOM_SERVER_FILE")
+  all_ids="${all_ids%?}"
 fi
 
+# Break SPECIFIC_SERVER_IDS into an array and validate whether each id exists in CUSTOM_SERVER_FILE
+IFS=',' read -ra SERVER_ID_ARRAY <<< "${SPECIFIC_SERVER_IDS:-$all_ids}"
+tests_to_run=()
+for id in "${SERVER_ID_ARRAY[@]}"; do
+  if [[ -z ${VALID_IDS[$id]} ]]; then
+    # SPECIFIC_SERVER_IDS is not in CUSTOM_SERVER_FILE
+    echo "ERROR: Server id $id is not found in CUSTOM_SERVER_FILE. Please check your configuration."
+    exit 1
+  else
+    tests_to_run+=($id)
+  fi
+done
+
+# If no valid server ids are found and USE_PUBLIC_TEST_SERVER is FALSE, then exit
+if [[ ${#tests_to_run[@]} -eq 0 ]] && [[ "${USE_PUBLIC_TEST_SERVER}" = "FALSE" ]]; then
+  echo "ERROR: No servers available for testing! This is due to the absence of valid server IDs and the setting of USE_PUBLIC_TEST_SERVER to FALSE. Please verify your CUSTOM_SERVER_FILE or alter USE_PUBLIC_TEST_SERVER to TRUE."
+  exit 1
+fi
+
+# If USE_PUBLIC_TEST_SERVER is TRUE, perform test on public server
+if [[ $USE_PUBLIC_TEST_SERVER = "TRUE" ]]; then
+  run_tests "$CUSTOM_ARGS"
+fi
+
+# Run tests for each valid id from SPECIFIC_SERVER_IDS
+for id in "${tests_to_run[@]}"; do
+  run_tests "--local-json $CUSTOM_SERVER_FILE --server $id $CUSTOM_ARGS"
+done
